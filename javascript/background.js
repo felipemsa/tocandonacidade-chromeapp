bfca10677402800ab9f6d0d2943ad6c209003476
felipemsa <felipe.fulgur@gmail.com>
2014-05-14 02:55:27 -0300

    v0.1

--- javascript/background.js
+++ javascript/background.js
@@ -1,14 +1,25 @@
 $(function() {
 	verifyNewSong();
+	$('a').click(function(){
+		window.open(this.href);
+	});
 });
 
 function verifyNewSong() {
+	if (localStorage.last) {
+		$('.song-info').eq(0).html(localStorage.last.split('-')[0] + '-' + localStorage.last.split('-')[1]);
+	}
 	$.ajax({
 			url: "http://www.radiocidade.fm/tools/marquee_cidade.html",
 			context: document.body
-		}).done(function(data) {
-			console.log("Tocando: ", data)
+		}).success(function(data) {
+			var strs = data.split('|');
+			var now = strs[1];
+			if (now && (!localStorage.last || localStorage.last != now)) {
+				localStorage.last = now;
+				$('.song-info').eq(0).html(now.split('-')[0] + '-' + now.split('-')[1]);
+			}
 		});
 
-	setTimeout(verifyNewSong, 5000);
+	setTimeout(verifyNewSong, 10000);
 }
