2e1f21d42ba1d2562e6d77e7affba8952c6f7dd6
felipemsa <felipe.fulgur@gmail.com>
2014-05-16 15:40:11 -0300

    versao 1.1

--- javascript/background.js
+++ javascript/background.js
@@ -1,13 +1,30 @@
 $(function() {
+	setTimeout(function() {
+	$('a').blur();
+	}, 200);
 	verifyNewSong();
 	$('a').click(function(){
 		window.open(this.href);
+	});
+	$('body').focus();
+	if (localStorage.notify == 'true') {
+		$('input[name=notify]').prop('checked', true);
+	} else {
+		$('input[name=notify]').prop('checked', false);
+	}
+	$('input[name=notify]').click(function() {
+		if (this.checked) {
+			localStorage.notify = true;
+		} else {
+			localStorage.notify = false;
+		}
 	});
 });
 
 function verifyNewSong() {
 	if (localStorage.last) {
-		$('.song-info').eq(0).html(localStorage.last.split('-')[0] + '-' + localStorage.last.split('-')[1]);
+		var music = localStorage.last.split('-')[0] + '-' + localStorage.last.split('-')[1];
+		$('.song-info').eq(0).html(music);
 	}
 	$.ajax({
 			url: "http://www.radiocidade.fm/tools/marquee_cidade.html",
@@ -17,9 +34,21 @@ function verifyNewSong() {
 			var now = strs[1];
 			if (now && (!localStorage.last || localStorage.last != now)) {
 				localStorage.last = now;
-				$('.song-info').eq(0).html(now.split('-')[0] + '-' + now.split('-')[1]);
+				var music = localStorage.last.split('-')[0] + '-' + localStorage.last.split('-')[1];
+				$('.song-info').eq(0).html(music);
+				if (localStorage.notify == 'true') {
+					var notification = webkitNotifications.createNotification('../images/ela-voltou.jpeg', 'Tocando na Cidade', music);
+					notification.show();
+					notification.onclick = function() {
+						window.open('http://www.radiocidade.fm/player.php');
+						notification.cancel();
+					};
+					setTimeout(function() {
+						notification.cancel();
+					}, 15000);
+				}
 			}
 		});
 
-	setTimeout(verifyNewSong, 10000);
+	setTimeout(verifyNewSong, 5000);
 }
